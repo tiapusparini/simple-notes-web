bae17e3dc2cb6ec8e26919c22d47c9738cf7130a
tiapusparini <tia.pusparini@gmail.com>
2021-12-30 18:08:22 +0700

    add tooltip and add dots on isi notes

--- src/pages/note/Note.js
+++ src/pages/note/Note.js
@@ -10,6 +10,7 @@ import {
   Card,
   CardActions,
   CardContent,
+  CardActionArea,
   Button,
   Container,
   ButtonGroup,
@@ -18,7 +19,7 @@ import {
   IconButton,
   Box,
 } from "@material-ui/core";
-import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 
 import {
   getNotes,
@@ -27,202 +28,259 @@ import {
   putNotes,
   postNotes,
 } from "../../functions/Notes";
-import {getPengguna} from "../../functions/Pengguna";
+import { getPengguna } from "../../functions/Pengguna";
 import { Typography } from "@material-ui/core";
 
 export default function Note() {
-    const history = useHistory();
-    const [state, setState] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [tambahState, setTambahState] = useState({
-      kodeWarna: "",
-      id_pengguna: "",
-      judul: "",
-      isi: "",
-    });
-    const [editState, setEditState] = useState({
-      id: "",
-      id_pengguna: "",
-      kodeWarna: "",
-      judul: "",
-      isi: "",
-    });
+  const history = useHistory();
+  const [state, setState] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [sliceString, setSliceString] = useState([]);
+  const [tambahState, setTambahState] = useState({
+    kodeWarna: "",
+    id_pengguna: "",
+    judul: "",
+    isi: "",
+  });
+  const [editState, setEditState] = useState({
+    id: "",
+    id_pengguna: "",
+    kodeWarna: "",
+    judul: "",
+    isi: "",
+  });
 
-    //LOAD EVERYTIME THE PAGE OPEN (DOESNT HAVE ANY DATA FROM DB YET)
-    useEffect(() => {
-      async function getData() {
-        const data = await getNotesByIdPengguna(localStorage.id);
-        let result = [];
-
-        data.forEach((x, i) => {
-          x = { ...x, 
-            no: i + 1,
-            updatedAt: new Date(x.updatedAt).toLocaleDateString("en-GB")
-          };
-          result.push(x);
-        });
-        setState(result);
-        setIsLoading(false);
-      }
-      getData();
-    }, []);
-    
-    const getDataNotes = async () => {
-      const data = await getNotesByIdPengguna(localStorage.id);
+  //LOAD EVERYTIME THE PAGE OPEN (DOESNT HAVE ANY DATA FROM DB YET)
+  useEffect(() => {
+    async function getData() {
+      const data = await getNotesByIdPengguna(localStorage.getItem("id"));
       let result = [];
-  
+
       data.forEach((x, i) => {
-        x = { ...x, 
+        x = {
+          ...x,
           no: i + 1,
-          updatedAt: new Date(x.updatedAt).toLocaleDateString("en-GB")
+          updatedAt: new Date(x.updatedAt).toLocaleDateString("en-GB"),
         };
         result.push(x);
       });
-
       setState(result);
       setIsLoading(false);
-    };
-    
-    const tambahNote = async () => {
-      setIsLoading(true);
-      const response = await postNotes(tambahState);
-      if (response.errorMessage === null) {
-        history.push(`/app/note`);
-      }
-      getDataNotes();
-      setIsLoading(false);
-      setTambahState({ judul: "", isi: "" , kodeWarna: ""});
-    };
+    }
+    getData();
+  }, []);
 
-    const editNotes = async () => {
-      setIsLoading(true);
-      const response = await putNotes(editState);
-      if (response.errorMessage === null) {
-        history.push(`/app/note`);
-      }
-      getDataNotes();
-      setIsLoading(false);
-      setEditState({
-        id: "",
-        id_pengguna: "",
-        judul: "",
-        isi: "",
-        kodeWarna: ""
-      });
-    };
+  const getDataNotes = async () => {
+    const data = await getNotesByIdPengguna(localStorage.getItem("id"));
+    let result = [];
 
-    return (
-      <Container>
+    data.forEach((x, i) => {
+      x = {
+        ...x,
+        no: i + 1,
+        updatedAt: new Date(x.updatedAt).toLocaleDateString("en-GB"),
+      };
+      result.push(x);
+    });
+
+    setState(result);
+    setIsLoading(false);
+  };
+
+  function countString(isi) {
+    let length = isi.length;
+
+    if (length >= 89) {
+      let dot = "...";
+      const text = isi.slice(0, 89);
+      const textConcat = text.concat(dot);
+      return textConcat;
+    } 
+    else {
+      const textConcat = isi;
+      return textConcat;
+    }
+  };
+
+  const tambahNote = async () => {
+    setIsLoading(true);
+    const response = await postNotes(tambahState);
+    if (response.errorMessage === null) {
+      history.push(`/app/note`);
+    }
+    getDataNotes();
+    setIsLoading(false);
+    setTambahState({ judul: "", isi: "", kodeWarna: "" });
+  };
+
+  const editNotes = async () => {
+    setIsLoading(true);
+    const response = await putNotes(editState);
+    if (response.errorMessage === null) {
+      history.push(`/app/note`);
+    }
+    getDataNotes();
+    setIsLoading(false);
+    setEditState({
+      id: "",
+      id_pengguna: "",
+      judul: "",
+      isi: "",
+      kodeWarna: "",
+    });
+  };
+
+  return (
+    <Container>
       {/* make loading sign before we getData from database */}
       {isLoading ? (
         <div style={{ textAlign: "center" }}>
           <CircularProgress size={50} style={{ marginTop: 50 }} />
         </div>
       ) : (
-      <Grid container spacing={2}>
-        {/* If no data in database, it will show no data. Otherwise it will show all data notes */}
-        {state.length ? (
-          state.map(state=>(
-            <Grid item md={3} sm={6} xs={12}>
-              {/* SHOW ALL NOTES */}
-              <Card style={{backgroundColor: `${state.kodeWarna}`, color:"#222831"}}>
-                <CardContent key={state.id}>
-                  <Box display="flex" alignItems="center" justifyContent="space-between">
-                    <Typography variant="h5" gutterBottom>{state.judul}</Typography>
-                    <Typography variant="caption" gutterBottom>{state.updatedAt}</Typography>
-                  </Box>
-                  <Typography variant="subtitle1">{state.isi}</Typography>
-                </CardContent>
-                <CardActions>
-                  <CustomModalDetail
-                    handleEdit={() => {
-                      editNotes();
-                    }}
-                    handleInitialData={async () => {
-                      setEditState({
-                        id: state.id,
-                        id_pengguna: state.id_pengguna,
-                        judul: state.judul,
-                        isi: state.isi,
-                        kodeWarna: state.kodeWarna,
-                      });
-                    }}
+        <Grid container spacing={2}>
+          {/* If no data in database, it will show no data. Otherwise it will show all data notes */}
+          {state.length ? ( //flexWrap="row wrap" alignSelf="auto" justifyContent="space-around"
+            state.map((state) => (
+              // <Grid container flex-wrap="wrap-reverse" justifyContent="space-between"
+              // alignItems="flex-start" md={3} spacing={2}>
+              <Grid item md={3} sm={6} xs={12}>
+                {/* SHOW ALL NOTES */}
+                <Card
+                  style={{
+                    backgroundColor: `${state.kodeWarna}`,
+                    color: "#222831",
+                  }}
+                >
+                  <CardContent
+                    key={state.id}
+                    style={{ height: 150, overFlow: "scroll" }}
                   >
-                    <Box style={{ marginBottom: "10px" }}>
-                      <IconButton
+                    <Box
+                      display="flex"
+                      alignItems="center"
+                      justifyContent="space-between"
+                      style={{ minWidth: "min-content" }}
+                    >
+                      <Typography variant="h5" gutterBottom>
+                        {state.judul}
+                      </Typography>
+                      <Typography variant="caption">
+                        {state.updatedAt}
+                      </Typography>
+                    </Box>
+                    <Typography variant="subtitle1">
+                      {countString(state.isi)}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <CustomModalDetail
+                      handleEdit={() => {
+                        editNotes();
+                      }}
+                      handleInitialData={async () => {
+                        setEditState({
+                          id: state.id,
+                          id_pengguna: state.id_pengguna,
+                          judul: state.judul,
+                          isi: state.isi,
+                          kodeWarna: state.kodeWarna,
+                        });
+                      }}
+                    >
+                      <Box style={{ marginBottom: "10px" }}>
+                        <IconButton
+                          size="small"
+                          onClick={() => {
+                            const tempKodeWarna = "#9fd3c7";
+                            setEditState((c) => ({
+                              ...c,
+                              kodeWarna: tempKodeWarna,
+                            }));
+                          }}
+                        >
+                          <FiberManualRecordIcon style={{ color: "#9fd3c7" }} />
+                        </IconButton>
+                        <IconButton
+                          size="small"
+                          onClick={() => {
+                            const tempKodeWarna = "#a7bcb9";
+                            setEditState((c) => ({
+                              ...c,
+                              kodeWarna: tempKodeWarna,
+                            }));
+                          }}
+                        >
+                          <FiberManualRecordIcon style={{ color: "#a7bcb9" }} />
+                        </IconButton>
+                        <IconButton
+                          size="small"
+                          onClick={() => {
+                            const tempKodeWarna = "#dde0ab";
+                            setEditState((c) => ({
+                              ...c,
+                              kodeWarna: tempKodeWarna,
+                            }));
+                          }}
+                        >
+                          <FiberManualRecordIcon style={{ color: "#dde0ab" }} />
+                        </IconButton>
+                      </Box>
+                      <Divider />
+                      <br />
+                      <Typography variant="caption">Judul</Typography>
+                      <TextField
                         size="small"
-                        onClick={() => {
-                          const tempKodeWarna = "#9fd3c7";
-                          setEditState((c) => ({ ...c, kodeWarna: tempKodeWarna }));
-                        }}>
-                          <FiberManualRecordIcon style={{color:"#9fd3c7"}}/>
-                      </IconButton>
-                      <IconButton 
+                        style={{ marginBottom: "13px" }}
+                        fullWidth
+                        value={editState.judul}
+                        onChange={(e) => {
+                          const tempJudul = e.target.value;
+                          setEditState((c) => ({ ...c, judul: tempJudul }));
+                        }}
+                      />
+                      <Typography variant="caption">Catatan</Typography>
+                      <TextField
                         size="small"
-                        onClick={() => {
-                          const tempKodeWarna = "#a7bcb9";
-                          setEditState((c) => ({ ...c, kodeWarna: tempKodeWarna }));
-                        }}>
-                          <FiberManualRecordIcon style={{color:"#a7bcb9"}}/>
-                      </IconButton>
-                      <IconButton  
-                        size="small"
-                        onClick={() => {
-                          const tempKodeWarna = "#dde0ab";
-                          setEditState((c) => ({ ...c, kodeWarna: tempKodeWarna }));
-                        }}>
-                          <FiberManualRecordIcon style={{color:"#dde0ab"}}/>
-                      </IconButton>
-                    </Box><Divider /><br />
-                    <Typography variant="caption">Judul</Typography>
-                    <TextField
-                      size="small"
-                      style={{ marginBottom: "13px" }}
-                      fullWidth
-                      value={editState.judul}
-                      onChange={(e) => {
-                        const tempJudul = e.target.value;
-                        setEditState((c) => ({ ...c, judul: tempJudul }));
+                        fullWidth
+                        multiline
+                        value={editState.isi}
+                        onChange={(e) => {
+                          const tempIsi = e.target.value;
+                          setEditState((c) => ({ ...c, isi: tempIsi }));
+                        }}
+                        style={{ marginBottom: "20px" }}
+                      />
+                    </CustomModalDetail>
+                    <CustomModalHapus
+                      handleDelete={async () => {
+                        setIsLoading(true);
+                        await deleteNotes(state.id);
+                        getDataNotes();
+                        setIsLoading(false);
                       }}
                     />
-                    <Typography variant="caption">Catatan</Typography>
-                    <TextField
-                      size="small"
-                      fullWidth
-                      multiline
-                      value={editState.isi}
-                      onChange={(e) => {
-                        const tempIsi = e.target.value;
-                        setEditState((c) => ({ ...c, isi: tempIsi }));
-                      }}
-                      style={{ marginBottom: "20px" }}
-                    />
-                  </CustomModalDetail>
-                  <CustomModalHapus
-                    handleDelete={async () => {
-                      setIsLoading(true);
-                      await deleteNotes(state.id);
-                      getDataNotes();
-                      setIsLoading(false);
-                    }}
-                  />
-                </CardActions>
-              </Card>
-            </Grid>
-          ))
-        ):(
-          <Container fixed>
-            <Typography 
-            variant="h2" 
-            style={{  
-              height: '60vh', 
-              textAlign:'center', 
-              marginTop:'10px', 
-              color:'grey',
-              paddingTop: '170px' 
-            }}>Ayo buat catatan!</Typography>
-          </Container>
-        )}
+                  </CardActions>
+                </Card>
+              </Grid>
+              // </Grid>
+            ))
+          ) : (
+            <Container fixed>
+              <Typography
+                variant="h1"
+                style={{
+                  height: "60vh",
+                  textAlign: "center",
+                  marginTop: "10px",
+                  color: "grey",
+                  paddingTop: "200px",
+                }}
+              >
+                Ayo buat catatan!
+              </Typography>
+            </Container>
+          )}
           {/* Modal Tambah */}
           <CustomModalTambah
             handleTambah={() => {
@@ -230,7 +288,7 @@ export default function Note() {
             }}
             handleInitialData={async () => {
               setTambahState({
-                kodeWarna: "", //`${tambahState.kodeWarna}`
+                kodeWarna: "#dde0ab", //`${tambahState.kodeWarna}`
                 id_pengguna: localStorage.id,
                 judul: "",
                 isi: "",
@@ -238,36 +296,42 @@ export default function Note() {
             }}
           >
             <Box style={{ marginBottom: "10px" }}>
-              <IconButton 
+              <IconButton
                 size="small"
                 onClick={() => {
                   const tempKodeWarna = "#9fd3c7";
                   setTambahState((c) => ({ ...c, kodeWarna: tempKodeWarna }));
                   console.log(tempKodeWarna);
-                }}>
-                  <FiberManualRecordIcon style={{color:"#9fd3c7"}}/>
+                }}
+              >
+                <FiberManualRecordIcon style={{ color: "#9fd3c7" }} />
               </IconButton>
-              <IconButton 
+              <IconButton
                 size="small"
                 onClick={() => {
                   const tempKodeWarna = "#a7bcb9";
                   setTambahState((c) => ({ ...c, kodeWarna: tempKodeWarna }));
                   console.log(tempKodeWarna);
-                }}>
-                  <FiberManualRecordIcon style={{color:"#a7bcb9"}}/>
+                }}
+              >
+                <FiberManualRecordIcon style={{ color: "#a7bcb9" }} />
               </IconButton>
-              <IconButton  
+              <IconButton
                 size="small"
                 onClick={() => {
                   const tempKodeWarna = "#dde0ab";
                   setTambahState((c) => ({ ...c, kodeWarna: tempKodeWarna }));
                   console.log(tempKodeWarna);
-                }}>
-                  <FiberManualRecordIcon style={{color:"#dde0ab"}}/>
+                }}
+              >
+                <FiberManualRecordIcon style={{ color: "#dde0ab" }} />
               </IconButton>
-            </Box><Divider /><br />
+            </Box>
+            <Divider />
+            <br />
             <Typography variant="caption">Judul</Typography>
             <TextField
+              autoFocus
               size="small"
               // placeholder="Ketik disini"
               style={{ marginBottom: "13px" }}
@@ -290,10 +354,9 @@ export default function Note() {
               }}
               style={{ marginBottom: "20px" }}
             />
-            
           </CustomModalTambah>
-      </Grid>
+        </Grid>
       )}
-      </Container>
-    );
+    </Container>
+  );
 }
